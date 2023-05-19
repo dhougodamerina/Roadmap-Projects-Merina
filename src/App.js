afea3519869c765092a33e0382890bfa91375cbc
merina dhougoda <merina@outside.tech>
2023-05-19 22:04:02 +0545

    fixed the draw state and changed the variables name

--- src/App.js
+++ src/App.js
@@ -5,50 +5,77 @@ import Cross from "./components/cross";
 import Box from "./components/box";
 
 const App = () => {
-  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [gameBoard, setGameBoard] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
   const [isXTurn, setIsXTurn] = useState(true);
 
   const calculateWinner = () => {
-    const lines = [
+    const winnerLine = [
       [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
       [0, 3, 6],
       [1, 4, 7],
       [2, 5, 8],
-      [0, 4, 7],
+      [0, 4, 8],
       [2, 4, 6],
     ];
-    for (let logic of lines) {
-      const [a, b, c] = logic;
-      if (board[a] !== null && board[a] === board[b] && board[a] === board[c])
-        return board[a];
+    for (let logic of winnerLine) {
+      const [x, y, z] = logic;
+      if (
+        gameBoard[x] !== null &&
+        gameBoard[x] === gameBoard[y] &&
+        gameBoard[x] === gameBoard[z]
+      )
+        return gameBoard[x];
+    }
+    if (!gameBoard.includes("")) {
+      return "Draw";
     }
     return false;
   };
 
   const handleClick = (e) => {
     const index = e.target.id;
-    const copyState = [...board];
-    copyState[index] = isXTurn ? "X" : "0";
-    setBoard(copyState);
+    if (gameBoard[index] !== "") {
+      return;
+    }
+    const boardState = [...gameBoard];
+    boardState[index] = isXTurn ? "X" : "0";
+    setGameBoard(boardState);
     setIsXTurn(!isXTurn);
   };
   const winner = calculateWinner();
 
-  const resetGame = () => {
-    setBoard(["", "", "", "", "", "", "", "", ""]);
+  const restartGame = () => {
+    setGameBoard(["", "", "", "", "", "", "", "", ""]);
   };
   return (
     <div className=" m-0 p-0 flex flex-col items-center justify-center h-[100vh] bg-slate-700">
       {winner ? (
         <div className="flex text-center flex-col">
-          <h2 className=" text-2xl text-white m-4">
-            Player {winner} is the winner!!
-          </h2>
+          {winner === "Draw" ? (
+            <>
+              <h1 className=" text-2xl text-white m-4">Game Over!!</h1>
+              <h2 className=" text-2xl text-white m-4">It's a draw!!</h2>
+            </>
+          ) : (
+            <h2 className=" text-2xl text-white m-4">
+              Player {winner} is the winner!!
+            </h2>
+          )}
           <button
-            onClick={resetGame}
-            className=" text-lg border border-white px-4 py-2 text-white"
+            onClick={restartGame}
+            className=" text-lg border border-white px-4 py-2 text-white hover:bg-white hover:text-slate-700"
           >
             Play Again
           </button>
@@ -56,11 +83,12 @@ const App = () => {
       ) : (
         <>
           <h2 className=" text-2xl text-white m-4">
-            Player {isXTurn ? "X" : "0"} turn
+            It's player{" "}
+            <span className=" italic">{`'${isXTurn ? "X" : "0"}'`}</span> turn.
           </h2>
           <div>
             <div className=" w-[600px] h-[600px] flex flex-wrap">
-              {board.map((cell, index) => (
+              {gameBoard.map((cell, index) => (
                 <Box
                   key={index}
                   id={index}
